37df8f5767105520bd75b826c4d7fbd79e4d4417
Tibor Vass <tibor@docker.com>
2019-06-28 23:04:24 +0000

    gomod
    Signed-off-by: Tibor Vass <tibor@docker.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module foo
+
+go 1.12
+
+require github.com/tiborvass/dump v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tiborvass/dump"
+)
+
+func main() {
+	fmt.Println(dump.Version)
+}
